576e035d76f54736adb685a3542ec58baccce46d
smutlak <smutlak@gmail.com>
2015-12-12 02:19:09 -0800

    mdb

--- SRS/SRS/Database/Database.sql
+++ SRS/SRS/Database/Database.sql
@@ -71,9 +71,12 @@ insert into sys_role(sysrole_id, sysrole_name, sysrole_desc) values(1, 'admins',
 insert into sys_role(sysrole_id, sysrole_name, sysrole_desc) values(2, 'volunteers', 'the volunteers role');
 insert into sys_role(sysrole_id, sysrole_name, sysrole_desc) values(3, 'citizens', 'the citizens role');
 insert into sys_user values (1,'admin','admin','1', GETDATE(), 1);
-insert into sys_user values (2,'volunteerA','admin','1', GETDATE(), 2);
-insert into sys_user values (3,'volunteerB','admin','1', GETDATE(), 2);
-insert into sys_user values (4,'volunteerC','admin','1', GETDATE(), 2);
+insert into sys_user values (2,'v1','v1','1', GETDATE(), 2);
+insert into sys_user values (3,'v2','v2','1', GETDATE(), 2);
+insert into sys_user values (4,'v3','v3','1', GETDATE(), 2);
+insert into sys_user values (5,'c1','c1','1', GETDATE(), 3);
+insert into sys_user values (6,'c2','c2','1', GETDATE(), 3);
+insert into sys_user values (7,'c3','c3','1', GETDATE(), 3);
 
 Select usr.sysuser_name, r.sysrole_name
 from [sys_user] usr inner join [sys_role] r on r.sysrole_id = usr.sysuser_role_id
@@ -82,3 +85,6 @@ where usr.sysuser_name='admin' and usr.sysuser_password ='admin'
 insert into sys_user values ((select max(sysuser_id)+1 from sys_user),'volunteerC','admin','1', GETDATE(), 2);
 
 
+Select * from sys_user
+
+delete from sys_user where sysuser_id = 5
